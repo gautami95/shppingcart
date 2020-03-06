0a9837c65a20ccbe35617d06e1ae60734c2ac154
Siddhesh93 <59689460+Siddhesh93@users.noreply.github.com>
2020-03-06 16:45:47 +0530

    updated 6/3/2020

---
+++ src/main/java/com/Pages/HomePage.java
@@ -0,0 +1,9 @@
+package com.Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends LoginOR{
+	public HomePage(WebDriver driver){
+		super(driver);
+	}
+}
---
+++ src/main/java/com/Pages/LoginOR.java
@@ -0,0 +1,12 @@
+package com.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.PageObject.PageObject;
+
+public class LoginOR extends PageObject {
+
+
+}
